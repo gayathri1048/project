39997841b030a3abae33a61e74d294fb7a6dc647
gayathri1048 <gayathrimuthumula@gmail.com>
2023-04-29 20:26:37 +0530

    f1

---
+++ question1.py
@@ -0,0 +1,6 @@
+nbook1=int(input())
+nbook2=int(input())
+nbook3=int(input())
+bcost=(nbook1*499)+(nbook2*855)+(nbook3*645)
+total_amount=bcost+((bcost/100)*12)+250
+print(total_amount)
---
+++ question2.py
@@ -0,0 +1,4 @@
+name=input()
+name=set(name)
+for ch in name:
+    print(ch,end=",")
